acb0ce19eb6e47a3a667bbcaf641c23c0e25d1d5
Rafael Santos <rafasantos321.rs58@gmail.com>
2021-05-19 22:25:20 -0300

    Término da pilha

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+main()
+{
+
+    empilhar(1);
+    empilhar(2);
+    empilhar(3);
+
+    exibir();
+
+    printf("\n");
+    /*printf("Desenpilhando ");
+    printf("\n%i\n", desempilhar());*/
+    
+    esvaziar();
+    printf("----------------------\n");
+    exibir();
+
+
+    system("pause");
+}
+//g++ -g -o out.exe main.c pilha.c
